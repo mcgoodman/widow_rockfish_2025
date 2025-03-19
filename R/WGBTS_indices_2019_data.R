d30128d3e2cc8b656271b653e6854be6aa2877cc
lauriebalstad <91223414+lauriebalstad@users.noreply.github.com>
2025-03-19 15:57:33 -0700

    survey index plots & ss3 updates

--- R/WGBTS_indices_2019_data.R
+++ R/WGBTS_indices_2019_data.R
@@ -1,7 +1,11 @@
-####### pULL AND PLOT INDICES
+library(dplyr)
+library(cowplot)
+library(ggplot2)
+
+####### pULL AND PLOT INDICES from 2019 assessment
 
 #Update with path
-dat_file_path <- "widow-assessment-update/data_provided/2019_assessment"
+dat_file_path <- "models/2019 base model/Base_45"
 
 
 #generate the data list
@@ -21,7 +25,7 @@ tibble(widow_indices)%>%
   filter(year >= 1900)%>% #remove the VAST index (negative years)
   filter(index == 8)%>%
   mutate(index = "NWFSC")%>% 
- # mutate(obs = obs/exp(lnQ_nwfsc))%>%#NWFSC
+  # mutate(obs = obs/exp(lnQ_nwfsc))%>%#NWFSC
   ggplot(aes(x = year, y = obs, col = as.factor(index)))+
   geom_point()+
   geom_errorbar(aes(x = year, ymin = qlnorm(.025,log(obs), sd = se_log) ,  #se in log space so convert
@@ -45,3 +49,88 @@ tibble(widow_indices) %>%
   ggtitle("NWFSC index 2003 - 2019", 
           subtitle = "VAST vs sdmTMB")+
   theme_minimal()
+
+### now get modern indices to plot/comp with
+
+# sdmTMB-based biomass index ------------------------------
+index_gm <- read.csv("data_provided/WCGBTS/delta_gamma/index/est_by_area.csv")
+index_gm$source <- "sdmTMB delta-\ngamma, 2025"
+index_gm <- index_gm %>% filter(area == "Coastwide")
+
+index_ln <- read.csv("data_provided/WCGBTS/delta_lognormal/index/est_by_area.csv")
+index_ln$source <- "sdmTMB delta-\nlognorm, 2025"
+index_ln <- index_ln %>% filter(area == "Coastwide")
+
+# reorganize old data
+old_index <- widow_indices %>% filter(index == 8) %>%
+  mutate(model = factor(ifelse(year < 1900, "GLMM", "VAST")))
+old_index$source <- old_index$model
+old_index$year <- abs(old_index$year)
+
+# plotting all the things... tho not sure it's good! ------
+# old data as plot 1
+dodge_width <- 0.75  # Adjust this value to control spacing
+old_comp <- ggplot(NULL, aes(x = year, col = source)) + 
+  geom_errorbar(data = old_index, aes(ymin = qlnorm(.025, log(obs), sd = se_log), 
+                                      ymax = qlnorm(.975, log(obs), sd = se_log)), 
+                position = position_dodge(width = dodge_width)) + 
+  geom_point(data = old_index, aes(y = obs), 
+             position = position_dodge(width = dodge_width)) + 
+  # geom_line(data = rbind(index_gm, index_ln), aes(y = est), lwd = 0.8) +
+  theme_bw() + theme(legend.position = "none") + 
+  labs(y = "index") + 
+  scale_color_manual(values = c("gray", "black"))
+
+# new data v. VAST as plot 2
+# combine the data frames
+index_ln$obs <- index_ln$est; index_ln$se_log <- index_ln$se
+index_gm$obs <- index_gm$est; index_gm$se_log <- index_gm$se
+all_dat <- rbind(old_index[, c(1, 4, 5, 7)] %>% filter(source == "VAST"),index_ln[, c(2, 10, 11, 9)], index_gm[, c(2, 10, 11, 9)])
+
+dodge_width <- 0.9  # Adjust this value to control spacing
+new_comp <- ggplot(all_dat, aes(year, obs, col = source)) + 
+  geom_errorbar(aes(ymin = qlnorm(.025, log(obs), sd = se_log),
+                    ymax = qlnorm(.975, log(obs), sd = se_log)),
+                position = position_dodge(width = dodge_width)) +
+  geom_point(position = position_dodge(width = dodge_width)) +
+  labs(y = "index") + 
+  theme_bw() + theme(legend.position = "bottom") + 
+  scale_color_manual(values = c("black", "#26828e", "#6ece58"))
+
+plot_grid(old_comp, new_comp, nrow = 2, rel_heights = c(0.9, 1))
+
+# juvenile index ------------------------------
+# new index
+juv_index <- read.csv("data_provided/RREAS/widow_indices.csv")
+colnames(juv_index) <- c("year", "obs", "se_log")
+juv_index$source <- rep("Update")
+# get the old index from the base model
+juv_old <- widow_indices %>% filter(index == 6) %>%
+  mutate(model = factor(ifelse(year < 1900, "Past unused", "Past used")))
+juv_old$source <- juv_old$model
+juv_old$year <- abs(juv_old$year)
+# merge
+juv_dat <- rbind(juv_index, juv_old[,c(1,4,5,7)])
+
+# plot 1 is unlogged
+dodge_width <- 0.9  # Adjust this value to control spacing
+no_log_plot <- ggplot(juv_dat, aes(year, obs, col = source)) + 
+  geom_errorbar(aes(ymin = qlnorm(.025, log(obs), sd = se_log),
+                    ymax = qlnorm(.975, log(obs), sd = se_log)),
+                position = position_dodge(width = dodge_width)) +
+  geom_point(position = position_dodge(width = dodge_width)) +
+  labs(y = "index") + 
+  theme_bw() + theme(legend.position = "none") + 
+  scale_color_manual(values = c("gray", "black", "#fde725"))
+
+# plot 2 is logged
+log_plot <- ggplot(juv_dat, aes(year, log(obs), col = source)) + 
+  geom_errorbar(aes(ymin = qlnorm(.025, log(obs), sd = exp(se_log)),
+                    ymax = qlnorm(.975, log(obs), sd = exp(se_log))),
+                position = position_dodge(width = dodge_width)) +
+  geom_point(position = position_dodge(width = dodge_width)) +
+  labs(y = "log(index)") + 
+  theme_bw() + theme(legend.position = "bottom") + 
+  scale_color_manual(values = c("gray", "black", "#fde725"))
+
+plot_grid(no_log_plot, log_plot, nrow = 2, rel_heights = c(0.9, 1))
--- R/model_comparison.R
+++ R/model_comparison.R
@@ -271,9 +271,219 @@ replist <- SS_output(
 SS_plots(replist, dir = catch_update_dir, printfolder = "R_Plots")
 
 
-# WCGBTS update ---------------------------------------------------------------
+# WCGBTS update: LN ---------------------------------------------------------------
+## Create outputs by copying and modifying Base_45_new ------------------------
 
-# TO-DO
+update_index_dir <- here(wd, "models", "data_updates", "index_update")
+
+if (!dir.exists(update_index_dir)) {
+  
+  dir.create(update_index_dir, recursive = TRUE)
+  
+  r4ss::copy_SS_inputs(base_ctl_dir, update_index_dir)
+  
+  files_rename <- list.files(update_index_dir)[grepl("2019", list.files(update_index_dir))]
+  
+  lapply(files_rename, \(x) {
+    file.rename(file.path(update_index_dir, x), gsub("2019", "2025", file.path(update_index_dir, x)))
+  })
+  
+}
+
+# Update forecast.ss
+fcast_2025 <- SS_readforecast(here(update_index_dir, "forecast.ss"), verbose = FALSE)
+fcast_2025$ForeCatch$year <- fcast_2025$ForeCatch$year + 6
+fcast_2025$Flimitfraction_m$year <- fcast_2025$Flimitfraction_m$year + 6
+SS_writeforecast(fcast_2025, update_index_dir, overwrite = TRUE)
+
+# Update control file
+ctl <- readLines(here(update_index_dir, "2025widow.ctl"))
+ctl <- gsub("2019widow", "2025widow", ctl)
+writeLines(ctl, here(update_index_dir, "2025widow.ctl"))
+
+# Update starter file
+strt <- readLines(here(update_index_dir, "starter.ss"))
+strt <- gsub("2019widow", "2025widow", strt)
+writeLines(strt, here(update_index_dir, "starter.ss"))
+
+# update data file
+data_2025 <- SS_readdat(here(update_index_dir, "2025widow.dat"))
+# end year update
+if(data_2025$endyr != 2024){
+  data_2025$endyr <- 2024
+}
+# survey 1 update
+wcgbts <- read.csv(here("data_provided", "WCGBTS", "delta_lognormal", "index", "est_by_area.csv")) %>% filter(area == "Coastwide")
+wcgbts_dat <- data.frame(year = wcgbts$year, 
+                         month = rep(8.8), 
+                         index = rep(8), 
+                         obs = wcgbts$est, 
+                         se_log = wcgbts$se)
+# survey 2 update
+juvsurv <- read.csv(here("data_provided", "RREAS", "widow_indices.csv"))
+juvsurv_dat <- data.frame(year = juvsurv$YEAR, 
+                          month = rep(7), 
+                          index = rep(6),
+                          obs = juvsurv$est, 
+                          se_log = juvsurv$logse)
+# for the index update, we want to keep the results from 1, 2, 3, 7, 9 and update 6 (juvsurv) and 8 (wcgbts)
+index_old <- data_2025$CPUE %>% filter(index %in% c(1, 2, 3, 7, 9)) # keep the old index information
+index_fin <- rbind(index_old, juvsurv_dat, wcgbts_dat)
+
+data_2025$CPUE <- index_fin
+
+SS_writedat(data_2025, here(update_index_dir, "2025widow.dat"), overwrite = TRUE)
+
+## Run ------------------------------------------------------------------------
+
+ss3_exe <- set_ss3_exe(update_index_dir)
+
+r4ss::run(
+  dir = update_index_dir,
+  exe = ss3_exe,
+  # extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = !rerun
+)
+
+replist <- SS_output(
+  dir = update_index_dir,
+  verbose = TRUE,
+  printstats = TRUE,
+  covar = TRUE
+)
+
+SS_plots(replist, dir = update_index_dir, printfolder = "R_Plots")
+
+# WCGBTS update: GM ---------------------------------------------------------------
+## Create outputs by copying and modifying Base_45_new ------------------------
+
+update_index_dir_gamma <- here(wd, "models", "data_updates", "index_update_gamma")
+
+if (!dir.exists(update_index_dir_gamma)) {
+  
+  dir.create(update_index_dir_gamma, recursive = TRUE)
+  
+  r4ss::copy_SS_inputs(base_ctl_dir, update_index_dir_gamma)
+  
+  files_rename <- list.files(update_index_dir_gamma)[grepl("2019", list.files(update_index_dir_gamma))]
+  
+  lapply(files_rename, \(x) {
+    file.rename(file.path(update_index_dir_gamma, x), gsub("2019", "2025", file.path(update_index_dir_gamma, x)))
+  })
+  
+}
+
+# Update forecast.ss
+fcast_2025 <- SS_readforecast(here(update_index_dir_gamma, "forecast.ss"), verbose = FALSE)
+fcast_2025$ForeCatch$year <- fcast_2025$ForeCatch$year + 6
+fcast_2025$Flimitfraction_m$year <- fcast_2025$Flimitfraction_m$year + 6
+SS_writeforecast(fcast_2025, update_index_dir_gamma, overwrite = TRUE)
+
+# Update control file
+ctl <- readLines(here(update_index_dir_gamma, "2025widow.ctl"))
+ctl <- gsub("2019widow", "2025widow", ctl)
+writeLines(ctl, here(update_index_dir_gamma, "2025widow.ctl"))
+
+# Update starter file
+strt <- readLines(here(update_index_dir_gamma, "starter.ss"))
+strt <- gsub("2019widow", "2025widow", strt)
+writeLines(strt, here(update_index_dir_gamma, "starter.ss"))
+
+# update data file
+data_2025 <- SS_readdat(here(update_index_dir_gamma, "2025widow.dat"))
+# end year update
+if(data_2025$endyr != 2024){
+  data_2025$endyr <- 2024
+}
+# survey 1 update
+wcgbts <- read.csv(here("data_provided", "WCGBTS", "delta_gamma", "index", "est_by_area.csv")) %>% filter(area == "Coastwide")
+wcgbts_dat <- data.frame(year = wcgbts$year, 
+                         month = rep(8.8), 
+                         index = rep(8), 
+                         obs = wcgbts$est, 
+                         se_log = wcgbts$se)
+# survey 2 update
+juvsurv <- read.csv(here("data_provided", "RREAS", "widow_indices.csv"))
+juvsurv_dat <- data.frame(year = juvsurv$YEAR, 
+                          month = rep(7), 
+                          index = rep(6),
+                          obs = juvsurv$est, 
+                          se_log = juvsurv$logse)
+# for the index update, we want to keep the results from 1, 2, 3, 7, 9 and update 6 (juvsurv) and 8 (wcgbts)
+index_old <- data_2025$CPUE %>% filter(index %in% c(1, 2, 3, 7, 9)) # keep the old index information
+index_fin <- rbind(index_old, juvsurv_dat, wcgbts_dat)
+
+data_2025$CPUE <- index_fin
+
+SS_writedat(data_2025, here(update_index_dir_gamma, "2025widow.dat"), overwrite = TRUE)
+
+## Run ------------------------------------------------------------------------
+
+ss3_exe <- set_ss3_exe(update_index_dir_gamma)
+
+r4ss::run(
+  dir = update_index_dir_gamma,
+  exe = ss3_exe,
+  # extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = !rerun
+)
+
+replist <- SS_output(
+  dir = update_index_dir_gamma,
+  verbose = TRUE,
+  printstats = TRUE,
+  covar = TRUE
+)
+
+SS_plots(replist, dir = update_index_dir_gamma, printfolder = "R_Plots")
+
+# WCGBTS + catch update ---------------------------------------------------------------
+## Create outputs by copying and modifying index_update version -----------------------
+
+update_all_dir <- here(wd, "models", "data_updates", "catch_index_update")
+
+if (!dir.exists(update_all_dir)) {
+  
+  dir.create(update_all_dir, recursive = TRUE)
+  
+  r4ss::copy_SS_inputs(update_index_dir, update_all_dir)
+  
+}
+
+# update data file to also have catches
+data_2025 <- SS_readdat(here(update_all_dir, "2025widow.dat"))
+# end year update
+if(data_2025$endyr != 2024){
+  data_2025$endyr <- 2024
+}
+# Replace catch with updated catch data file created by R/catches.R
+data_2025 <- SS_readdat(here(update_all_dir, "2025widow.dat"))
+data_2025$catch <- read.csv(here("data_derived", "catches", "2025_catches.csv"))
+data_2025$Comments[1] <- "#C 2025 Widow Rockfish Update Assessment"
+SS_writedat(data_2025, here(update_all_dir, "2025widow.dat"), overwrite = TRUE)
+
+## Run ------------------------------------------------------------------------
+
+ss3_exe <- set_ss3_exe(update_all_dir)
+
+r4ss::run(
+  dir = update_all_dir,
+  exe = ss3_exe,
+  # extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = !rerun
+)
+
+replist <- SS_output(
+  dir = update_all_dir,
+  verbose = TRUE,
+  printstats = TRUE,
+  covar = TRUE
+)
+
+SS_plots(replist, dir = update_all_dir, printfolder = "R_Plots")
 
 # Model comparison plots ------------------------------------------------------
 
@@ -308,21 +518,32 @@ SSplotComparisons(
   filenameprefix = "sens_base_adj_"
 )
 
-(par_cor <- cor(Models_SS$pars[,paste0("replist", 1:5)], use = "pairwise.complete.obs"))
+<<<<<<< HEAD
+## Compare 2019 base model runs to those with updated WCGBTS data -----
+
+# List directories
+folders <- c(here(wd, 'models', '2019 base model', c("Base_45", "Base_45_new")), update_index_dir, update_index_dir_gamma)
+=======
+  (par_cor <- cor(Models_SS$pars[,paste0("replist", 1:5)], use = "pairwise.complete.obs"))
 
 ## Compare 2019 base model runs to those with updated catch / WCGBTS data -----
 
 # List directories
 folders <- here(wd, 'models', '2019 base model', "Base_45_trawl_adj") # TO-DO: change reference data to reflect trawl adj.
 folders <- c(folders, here(wd, 'models', "data_updates", "update_catch")) # TO-DO: add WCGBTS update
+>>>>>>> 37052b0411d6b6ff918afd8cd96072a2a9a1cbb3
 
 #Comparison plots produced by r4ss
 Models <- SSgetoutput(dirvec = folders, getcovar = TRUE)
 Models_SS <- SSsummarize(Models)
 
-dir.create(plot_dir <- here(wd, 'models', 'compare_plots', "data_update"), recursive = TRUE)
+dir.create(plot_dir <- here(wd, 'models', 'compare_plots', "compare_index_update"), recursive = TRUE)
 
-mod_names <- c("base + mid/bot trawl adj.", "updated catches") #add model names as appropriate
+<<<<<<< HEAD
+mod_names <- c('base', 'new ctl', '+ ln index', '+ gm index') #add model names as appropriate
+=======
+  mod_names <- c("base + mid/bot trawl adj.", "updated catches") #add model names as appropriate
+>>>>>>> 37052b0411d6b6ff918afd8cd96072a2a9a1cbb3
 
 #plot time series (SSB, Recruitment, Fishing mortality)
 SSplotComparisons(
@@ -333,5 +554,30 @@ SSplotComparisons(
   densitynames = c("SSB_2019", "SSB_Virgin"),
   legendlabels = mod_names,
   indexPlotEach = TRUE,
-  filenameprefix = "sens_data_update"
+  filenameprefix = "comp_index_update_"
+)
+
+## Compare 2019 base model runs, catch update, index update, index/catch update -----
+
+# List directories
+folders <- c(here(wd, 'models', '2019 base model', c("Base_45", "Base_45_new")), catch_update_dir, update_index_dir, update_all_dir)
+
+#Comparison plots produced by r4ss
+Models <- SSgetoutput(dirvec = folders, getcovar = TRUE)
+Models_SS <- SSsummarize(Models)
+
+dir.create(plot_dir <- here(wd, 'models', 'compare_plots', "both_catch_index_update"), recursive = TRUE)
+
+mod_names <- c('base', 'new ctl', "+ catch update", "+ index update", "+ catch/index updates") #add model names as appropriate
+
+#plot time series (SSB, Recruitment, Fishing mortality)
+SSplotComparisons(
+  Models_SS,
+  print = TRUE,
+  plotdir = plot_dir,
+  labels = labels,
+  densitynames = c("SSB_2019", "SSB_Virgin"),
+  legendlabels = mod_names,
+  indexPlotEach = TRUE,
+  filenameprefix = "sens_catch_index_update_"
 )

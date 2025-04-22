f96c1c64f7a74d2eeb453646e2d3a7b536dc9454
Maurice Goodman <goodman.maurice@gmail.com>
2025-04-22 18:00:18 -0400

    working on model bridging

--- R/functions/bridging_functions.R
+++ R/functions/bridging_functions.R
@@ -1,35 +1,124 @@
+
+
+#' Function to install SS3 on OSX X68
+#' @param dir directory to install SS3 in
+#' @param url URL for SS3 OSX x86 executable
+#' @param ... Other arguments to `r4ss::get_ss3_exe`
+#'
+#' @return string with ss executable name
+set_ss_osx <- function(dir, url="https://github.com/nmfs-ost/ss3-source-code/releases/download/v3.30.23.1/ss3_osx", ...) {
+  
+  # Get and set filename for SS3 exe
+  ss3_exe <- c("ss_osx")
+  ss3_check <- vapply(ss3_exe, \(x) file.exists(file.path(dir, paste0(x, ".exe"))), logical(1))
+  if (!any(ss3_check)) utils::download.file(url, destfile = file.path(dir, "ss3"), mode = "wb") #r4ss::get_ss3_exe(dir, ...)
+  ss3_exe <- ss3_exe[which(vapply(ss3_exe, \(x) file.exists(file.path(dir, paste0(x, ".exe"))), logical(1)))]
+  return("ss_osx")
+}
+
+#' Wrapper for r4ss::get_ss3_exe to check for, download, and return name of SS3 exe file
+#' @param dir directory to install SS3 in
+#' @param ... Other arguments to `r4ss::get_ss3_exe`
+#'
+#' @return Character string with name of downloaded SS3 exe (without extension)
+set_ss3_exe <- function(dir, ...) {
+  
+  if (grepl("x86", sessionInfo()$R.version$platform) & grepl("darwin", sessionInfo()$R.version$os)) {
+    set_ss_osx(dir, ...)
+  } else {
+    # Get and set filename for SS3 exe
+    ss3_exe <- c("ss", "ss3")
+    ss3_check <- vapply(ss3_exe, \(x) file.exists(file.path(dir, paste0(x, ".exe"))), logical(1))
+    if (!any(ss3_check)) r4ss::get_ss3_exe(dir, ...)
+    ss3_exe <- ss3_exe[which(vapply(ss3_exe, \(x) file.exists(file.path(dir, paste0(x, ".exe"))), logical(1)))]
+    return(ss3_exe)
+  }
+  
+}
+
+#' Function to add new row to parameters by copying another row
+#'
+#' @param data Data frame (with row names)
+#' @param new_row name of new row to add
+#' @param ref_row name of reference row to copy values from
+#' @param ... Values to override reference with (e.g. )
+#'
+#' @examples
+#' 
+#' mtcars |> 
+#'   insert_row(
+#'     new_row = "Mazda RX4 Wagu", 
+#'     ref_row = "Maxda RX4 Wag",
+#'     mpg = 100
+#'   )
+#' 
+insert_row <- function(data, new_row, ref_row, ...) {
+  
+  rownames <- row.names(data)
+  
+  stopifnot(ref_row %in% rownames(data))
+  stopifnot(length(new_row) == 1)
+  stopifnot(length(ref_row) == 1)
+  stopifnot(all(names(list(...)) %in% names(data)))
+  
+  ref_clone <- data[ref_row,]
+  rownames(ref_clone) <- new_row
+  ref_clone <- modifyList(ref_clone, list(...))
+  
+  do.call(
+    "rbind", 
+    list(
+      data[1:which(rownames == ref_row),], 
+      ref_clone, 
+      data[(which(rownames == ref_row) + 1):nrow(data),]
+    )
+  )
+  
+}
+
 ##comapre function
 
-compare_ss3_mods <- function(dirs,replist = NULL, plot_dir,plot_names = NULL,...){
-
+compare_ss3_mods <- function(dirs, replist = NULL, plot_dir, plot_names = NULL, ...){
+  
   if(dir.exists(plot_dir)){
     file.remove(list.files(plot_dir,full.names = T)) #create a plot dir
     
   } else {
     dir.create(plot_dir) #create a plot dir
   }
-
-#If a replist is supplied, use that , otherwise read replists from the dirs
-if(is.null(replist)){
   
-  replist <- SSgetoutput(dirvec = dirs)
+  #If a replist is supplied, use that , otherwise read replists from the dirs
+  if(is.null(replist)){
+    
+    replist <- SSgetoutput(dirvec = dirs)
+  }
+  
+  mod_summ <- SSsummarize(biglist = replist) #summarise reports
+  
+  SSplotComparisons( #plot comparrisons
+    mod_summ,
+    print = TRUE,
+    plotdir = plot_dir,
+    legendlabels = plot_names,
+    indexPlotEach = TRUE,
+    ...
+  )
+  
+  return(plot_dir)
 }
 
-mod_summ <- SSsummarize(biglist = replist) #summarise reports
-
-SSplotComparisons( #plot comparrisons
-  mod_summ,
-  print = TRUE,
-  plotdir = plot_dir,
-  legendlabels = plot_names,
-  indexPlotEach = TRUE,
-  ...
-)
-
-return(plot_dir)
-}
-
-update_ss3_dat <- function(new_dir,base_dir = here(wd, 'models', '2019 base model', 'Base_45_new'),new_dat = NULL,datlist_name = NULL,assessment_yr,run_after_write = FALSE,ss_exe_loc = ss3_exe,...){
+#' One-time use function to update SS3 data file for data bridging
+#' @param new_dir 
+#' @param base_dir 
+#' @param new_dat 
+#' @param datlist_name 
+#' @param assessment_yr 
+#' @param run_after_write 
+#' @param ss_exe_loc 
+#' @param ... 
+#'
+#' @return silent
+update_ss3_dat <- function(new_dir, base_dir = here(wd, 'models', '2019 base model', 'Base_45_new'), new_dat = NULL, datlist_name = NULL, assessment_yr, run_after_write = FALSE, ss_exe_loc = ss3_exe, ...){
   
   temp <- SS_read(base_dir)
   mod_end_yr <- assessment_yr - 1 #data ends year previous
@@ -37,108 +126,108 @@ update_ss3_dat <- function(new_dir,base_dir = here(wd, 'models', '2019 base mode
   # data_temp <- new_dat[new_dat$year <= mod_end_yr,] #filter base on model end year
   # 
   # temp[["dat"]][[datlist_name]] <- data_temp #update the object based on data name
- if(!is.null(new_dat)){
-  for(i in 1:length(new_dat)){
-   dat <- new_dat[[i]]
-   data_temp <- dat|>filter(year <= mod_end_yr)
-   temp[["dat"]][[datlist_name[[i]]]] <- data_temp #update the object based on data name
-   data_temp <- NULL
- }
-
-}
+  if(!is.null(new_dat)){
+    for(i in 1:length(new_dat)){
+      dat <- new_dat[[i]]
+      data_temp <- dat|>filter(year <= mod_end_yr)
+      temp[["dat"]][[datlist_name[[i]]]] <- data_temp #update the object based on data name
+      data_temp <- NULL
+    }
+    
+  }
   
   
   temp[["dat"]]["Comments"][1] <- "#C 2025 Widow Rockfish Update Assessment"
-   #for extending the model
-    cat("base model and new model end years are different, updating input files...")
-    dir.create(new_dir)#create the dir
-    old_assesment_yr <- temp[["dat"]][["endyr"]] + 1
+  #for extending the model
+  cat("base model and new model end years are different, updating input files...")
+  dir.create(new_dir)#create the dir
+  old_assesment_yr <- temp[["dat"]][["endyr"]] + 1
+  
+  year_diff <- mod_end_yr-temp[["dat"]][["endyr"]]
+  temp[["dat"]][["endyr"]] <- mod_end_yr
+  temp[["dat"]][["endyr"]]
+  
+  #Update the miniumum sample sizes
+  temp[["dat"]][["age_info"]][["minsamplesize"]] <- 0.01
+  temp[["dat"]][["len_info"]][["minsamplesize"]] <- 0.01
+  
+  #Update and write the control file
+  ctl <- temp$ctl
+  ctl$MainRdevYrLast <- 2020
+  
+  ##Swap lambdas and variacne adjustmne tfactors
+  new_var_adj_df <- ctl$lambdas|>
+    select(like_comp,fleet,value)|>
+    rename(factor = like_comp,fleet = fleet)
+  
+  #Now set the lambdas to 1
+  ctl$lambdas <- ctl$lambdas|>
+    mutate(value = 1)
+  
+  #Now add var adjustment factors data type, fleet, value
+  ctl$DoVar_adjust <- 1 # turn on var adjust
+  ctl$Variance_adjustment_list <- new_var_adj_df#add vARIANCE ADJUSTMNE TLIST
+  
+  ##Add the timae varying selex for hake
+  if(ctl$N_Block_Designs != 11 ){
     
-    year_diff <- mod_end_yr-temp[["dat"]][["endyr"]]
-    temp[["dat"]][["endyr"]] <- mod_end_yr
-    temp[["dat"]][["endyr"]]
+    ctl$N_Block_Designs <- 11
+    ctl$blocks_per_pattern <- c(ctl$blocks_per_pattern ,"blocks_per_pattern_12" = 1)
+    ctl$Block_Design[[11]] <- c(1916,2018)
     
-    #Update the miniumum sample sizes
-    temp[["dat"]][["age_info"]][["minsamplesize"]] <- 0.01
-    temp[["dat"]][["len_info"]][["minsamplesize"]] <- 0.01
+    #Adjust the blocks on hake selx pars 1,2,3
+    old_sel_pars <- ctl$size_selex_parms[c("SizeSel_P_1_Hake(3)","SizeSel_P_2_Hake(3)","SizeSel_P_3_Hake(3)"),]
+    new_sel_pars <- old_sel_pars|>
+      mutate(Block = 11,
+             Block_Fxn = 2)
     
-    #Update and write the control file
-    ctl <- temp$ctl
-    ctl$MainRdevYrLast <- 2020
+    #replace the ctl model ones
     
-    ##Swap lambdas and variacne adjustmne tfactors
-    new_var_adj_df <- ctl$lambdas|>
-      select(like_comp,fleet,value)|>
-      rename(factor = like_comp,fleet = fleet)
-    
-    #Now set the lambdas to 1
-    ctl$lambdas <- ctl$lambdas|>
-      mutate(value = 1)
-    
-    #Now add var adjustment factors data type, fleet, value
-    ctl$DoVar_adjust <- 1 # turn on var adjust
-    ctl$Variance_adjustment_list <- new_var_adj_df#add vARIANCE ADJUSTMNE TLIST
-    
-    ##Add the timae varying selex for hake
-    if(ctl$N_Block_Designs != 11 ){
-      
-      ctl$N_Block_Designs <- 11
-      ctl$blocks_per_pattern <- c(ctl$blocks_per_pattern ,"blocks_per_pattern_12" = 1)
-      ctl$Block_Design[[11]] <- c(1916,2018)
-      
-      #Adjust the blocks on hake selx pars 1,2,3
-      old_sel_pars <- ctl$size_selex_parms[c("SizeSel_P_1_Hake(3)","SizeSel_P_2_Hake(3)","SizeSel_P_3_Hake(3)"),]
-      new_sel_pars <- old_sel_pars|>
-        mutate(Block = 11,
-               Block_Fxn = 2)
-      
-      #replace the ctl model ones
-      
-      #Add some to the trimmed verion
-      ctl$size_selex_parms[c("SizeSel_P_1_Hake(3)","SizeSel_P_2_Hake(3)","SizeSel_P_3_Hake(3)"),] <- new_sel_pars
-      ctl$size_selex_parms_tv <- rbind(ctl$size_selex_parms_tv[1:24,], #up to midwater sel pars
-                  new_sel_pars[,1:7], # new hake sel pars
-                  ctl$size_selex_parms_tv[25:29,]) #hnl onward sl pars
-      }
-    
-    SS_writectl(ctllist = ctl,here(new_dir,paste0(assessment_yr,"widow.ctl")),overwrite = T)
-    
-    #Correct names
-    ctl <- readLines(list.files(path = here::here(new_dir),pattern = "\\.ctl$",full.names = TRUE))
-    ctl <- gsub(paste0(old_assesment_yr,"widow"), "2025widow", ctl)
-    writeLines(ctl, here(new_dir, paste0(assessment_yr,"widow.ctl")))
-    
-
-    
-    #updat the starter file 
-    strt <- readLines(here(base_dir, "starter.ss"))
-    strt <- gsub(paste0(old_assesment_yr,"widow"), paste0(assessment_yr,"widow"), strt)
-    writeLines(strt, here(new_dir, "starter.ss"))
-    
-    #update the forecastr file
-    forc <- SS_readforecast(here(base_dir, "forecast.ss"))
-    temp[["fore"]][["ForeCatch"]][["year"]] <-  temp[["fore"]][["ForeCatch"]][["year"]]+year_diff
-    temp[["fore"]][["Flimitfraction_m"]][["year"]] <-  temp[["fore"]][["Flimitfraction_m"]][["year"]]+year_diff
-
-    r4ss::SS_writeforecast(temp[["fore"]],dir = new_dir,overwrite = TRUE)
-    
-    #update and write the data file
-    r4ss::SS_writedat(temp[["dat"]],outfile = here(new_dir,paste0(assessment_yr,"widow.dat")),overwrite= T)
+    #Add some to the trimmed verion
+    ctl$size_selex_parms[c("SizeSel_P_1_Hake(3)","SizeSel_P_2_Hake(3)","SizeSel_P_3_Hake(3)"),] <- new_sel_pars
+    ctl$size_selex_parms_tv <- rbind(ctl$size_selex_parms_tv[1:24,], #up to midwater sel pars
+                                     new_sel_pars[,1:7], # new hake sel pars
+                                     ctl$size_selex_parms_tv[25:29,]) #hnl onward sl pars
+  }
+  
+  SS_writectl(ctllist = ctl,here(new_dir,paste0(assessment_yr,"widow.ctl")),overwrite = T)
+  
+  #Correct names
+  ctl <- readLines(list.files(path = here::here(new_dir),pattern = "\\.ctl$",full.names = TRUE))
+  ctl <- gsub(paste0(old_assesment_yr,"widow"), "2025widow", ctl)
+  writeLines(ctl, here(new_dir, paste0(assessment_yr,"widow.ctl")))
+  
+  
+  
+  #updat the starter file 
+  strt <- readLines(here(base_dir, "starter.ss"))
+  strt <- gsub(paste0(old_assesment_yr,"widow"), paste0(assessment_yr,"widow"), strt)
+  writeLines(strt, here(new_dir, "starter.ss"))
+  
+  #update the forecastr file
+  forc <- SS_readforecast(here(base_dir, "forecast.ss"))
+  temp[["fore"]][["ForeCatch"]][["year"]] <-  temp[["fore"]][["ForeCatch"]][["year"]]+year_diff
+  temp[["fore"]][["Flimitfraction_m"]][["year"]] <-  temp[["fore"]][["Flimitfraction_m"]][["year"]]+year_diff
+  
+  r4ss::SS_writeforecast(temp[["fore"]],dir = new_dir,overwrite = TRUE)
+  
+  #update and write the data file
+  r4ss::SS_writedat(temp[["dat"]],outfile = here(new_dir,paste0(assessment_yr,"widow.dat")),overwrite= T)
   
   cat("Model files written to: ",new_dir)
-
+  
   #Set the executable
-file.copy(file.path(base_dir,"ss3.exe"),to = file.path(new_dir,"ss3.exe"))
-
- if(run_after_write == TRUE){
-   cat("running model...")
-   r4ss::run(
-     dir = new_dir,
-     exe = "ss3",
-     show_in_console = T,
-     ...
-   )
- }
+  file.copy(file.path(base_dir,"ss3.exe"),to = file.path(new_dir,"ss3.exe"))
+  
+  if(run_after_write == TRUE){
+    cat("running model...")
+    r4ss::run(
+      dir = new_dir,
+      exe = "ss3",
+      show_in_console = T,
+      ...
+    )
+  }
   
   
 }
@@ -153,10 +242,17 @@ file.copy(file.path(base_dir,"ss3.exe"),to = file.path(new_dir,"ss3.exe"))
 # compare_ss3_mods(dirs = c(base_dir,hist_catch_year,update_hist_catch_dir),plot_dir = here(bridge_2_dir,"hist_catch_year","compare_plots"),plot_names = c("base_45","fn hist catch","update_hist_catch"))
 # 
 
-### Function to re-weight comp data in stock synthesis model
-
-
-update_model_weights <- function(model_dir,new_weight_dir,weight_method = "MI",weight_mult = 0.5,exe = ss3_exe,expanded_fleets = c(1,2,3,4,5),run_after_write = TRUE){
+#' Function to re-weight comp data in stock synthesis model
+#' @param model_dir 
+#' @param new_weight_dir 
+#' @param weight_method 
+#' @param weight_mult 
+#' @param exe 
+#' @param expanded_fleets 
+#' @param run_after_write 
+#'
+#' @return Differences to console
+update_model_weights <- function(model_dir, new_weight_dir, weight_method = "MI", weight_mult = 0.5, exe = ss3_exe, expanded_fleets = 1:5, run_after_write = TRUE){
   
   #generate a report from the model
   rep <- r4ss::SS_output(model_dir)
@@ -177,7 +273,7 @@ update_model_weights <- function(model_dir,new_weight_dir,weight_method = "MI",w
   survey_lambdas <- old_lambdas|> 
     filter(!fleet %in% expanded_fleets)|>
     select(like_comp,fleet,value)
-    
+  
   
   
   new_lambdas <- new_tune_weights|>filter(fleet %in% expanded_fleets)|>
@@ -204,12 +300,12 @@ update_model_weights <- function(model_dir,new_weight_dir,weight_method = "MI",w
               extras = "-nohess",
               show_in_console = TRUE)
   }
-    
+  
   #Print the differences to the console
   compare_df <- new_lambdas|>
-  mutate(value = round(value,3))|>
+    mutate(value = round(value,3))|>
     rename(new_lambda = value)|>
-  cbind("old_lambda" = old_lambdas$value)
+    cbind("old_lambda" = old_lambdas$value)
   
   print(compare_df)
 }
---
+++ R/model_bridging.R
@@ -0,0 +1,223 @@
+
+library("tidyverse")
+library("r4ss")
+library("here")
+
+source(here("R", "functions", "bridging_functions.R"))
+
+# Base model ----------------------------------------------
+
+# This will be Mico's 2025 base model
+basedir <- here("models", "data_bridging", "finalised_data_bridging", "reweight_hnl_removed_reweighted")
+
+# Download SS3 exe and return name
+base_exe <- set_ss3_exe(basedir)
+
+# Run base model
+r4ss::run(
+  dir = basedir,
+  exe = base_exe,
+  extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = TRUE
+)
+
+SS_plots(
+  SS_output(basedir), 
+  dir = basedir, 
+  printfolder = "plots"
+)
+
+# Mortality -----------------------------------------------
+
+# Location for all model bridging runs
+dir.create(bridgedir <- here("models", "model_bridging"))
+
+# Copy over model files from base model to modify
+dir.create(Mdir <- here(bridgedir, "mortality"))
+r4ss::copy_SS_inputs(basedir, Mdir, overwrite = TRUE)
+Mexe <- set_ss3_exe(Mdir)
+
+# Read in control file to modify
+ctrl <- SS_readctl(paste0(Mdir, "/2025widow.ctl"), datlist = paste0(Mdir, "/2025widow.dat"))
+
+#max age from 2015 is 54
+maxA = 54
+#Hamel and Cope 2022 coefficient 
+a = 5.4
+logMean = log(a/maxA)
+
+#ctrl$MG_parms["NatM_p_1_Fem_GP_1", ]$PRIOR = logMean
+ctrl$MG_parms["NatM_p_1_Fem_GP_1", ]$PR_SD <- 0.31
+#ctrl$MG_parms["NatM_p_1_Mal_GP_1", ]$PRIOR = logMean
+ctrl$MG_parms["NatM_p_1_Mal_GP_1", ]$PR_SD <- 0.31
+
+SS_writectl(ctrl, paste0(Mdir, "/2025widow.ctl"), overwrite = TRUE)
+
+r4ss::run(
+  dir = Mdir,
+  exe = Mexe,
+  extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = TRUE
+)
+
+SS_plots(
+  SS_output(Mdir), 
+  dir = Mdir, 
+  printfolder = "plots"
+)
+
+# Length / weight parameters ------------------------------
+
+dir.create(LWdir <- here(bridgedir, "length_weight"))
+r4ss::copy_SS_inputs(Mdir, LWdir, overwrite = TRUE)
+LWexe <- set_ss3_exe(LWdir)
+
+ctrl <- SS_readctl(paste0(LWdir, "/2025widow.ctl"), datlist = paste0(LWdir, "/2025widow.dat"))
+
+ctrl$MG_parms["Wtlen_1_Fem_GP_1", ]$INIT <- 1.59e-5
+ctrl$MG_parms["Wtlen_2_Fem_GP_1", ]$INIT <- 2.99
+ctrl$MG_parms["Wtlen_1_Mal_GP_1", ]$INIT <- 1.45e-5
+ctrl$MG_parms["Wtlen_2_Mal_GP_1", ]$INIT <- 3.01
+
+SS_writectl(ctrl, paste0(LWdir, "/2025widow.ctl"), overwrite = TRUE)
+
+r4ss::run(
+  dir = LWdir,
+  exe = LWexe,
+  #extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = TRUE
+)
+
+LW_replist <- SS_output(LWdir)
+
+SS_plots(
+  LW_replist, 
+  dir = LWdir, 
+  printfolder = "plots"
+)
+
+# Recruitment bias ramp -----------------------------------
+
+dir.create(SRdir <- here(bridgedir, "SR_bias_adj"))
+r4ss::copy_SS_inputs(LWdir, SRdir, overwrite = TRUE)
+SRexe <- set_ss3_exe(SRdir)
+
+# Fit bias ramp
+dir.create(plot_dir <- here(SRdir, "plots"))
+png(paste0(plot_dir, "/biasRamp.png"))
+biasRamp = SS_fitbiasramp(LW_replist)
+dev.off()
+
+ctrl <- SS_readctl(paste0(SRdir, "/2025widow.ctl"), datlist = paste0(SRdir, "/2025widow.dat"))
+
+# Input bias ramp parameters
+# Last year of main recruitment deviations changed in data bridging
+ctrl$last_early_yr_nobias_adj = biasRamp$df[1,1]
+ctrl$first_yr_fullbias_adj = biasRamp$df[2,1]
+ctrl$last_yr_fullbias_adj = biasRamp$df[3,1]
+ctrl$first_recent_yr_nobias_adj = biasRamp$df[4,1]
+ctrl$max_bias_adj  = biasRamp$df[5,1]
+
+SS_writectl(ctrl, paste0(SRdir, "/2025widow.ctl"), overwrite = TRUE)
+
+r4ss::run(
+  dir = SRdir,
+  exe = SRexe,
+  extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = TRUE
+)
+
+SS_plots(
+  SS_output(SRdir, covar=TRUE), 
+  dir = SRdir, #basedir, 
+  printfolder = "plots", 
+  plot = c(1:21, 23:26)
+)
+
+# Midwater trawl blocks -----------------------------------
+
+# Midwater trawl selectivity (parameters 1, 3, 4, 6), 
+# and retention (parameter 3)
+
+dir.create(Block7dir <- here(bridgedir, "Block7"))
+r4ss::copy_SS_inputs(SRdir, Block7dir, overwrite = TRUE)
+Block7exe <- set_ss3_exe(Block7dir)
+
+ctrl <- SS_readctl(paste0(Block7dir, "/2025widow.ctl"), datlist = paste0(Block7dir, "/2025widow.dat"))
+
+# Add time block to block group
+ctrl$Block_Design[[7]] <- c(ctrl$Block_Design[[7]], c(2011, 2016))
+
+# Note that there is an additional block in block group
+ctrl$blocks_per_pattern[7] <- ctrl$blocks_per_pattern[7] + 1
+
+# Add rows to time-varying selectivity for relevant parameters
+ctrl$size_selex_parms_tv <- 
+  ctrl$size_selex_parms_tv |> 
+  insert_row(
+    new_row = "SizeSel_P_1_MidwaterTrawl(2)_BLK7repl_2011",
+    ref_row = "SizeSel_P_1_MidwaterTrawl(2)_BLK7repl_2002"
+  ) |> 
+  insert_row(
+    new_row = "SizeSel_P_3_MidwaterTrawl(2)_BLK7repl_2011",
+    ref_row = "SizeSel_P_3_MidwaterTrawl(2)_BLK7repl_2002"
+  ) |> 
+  insert_row(
+    new_row = "SizeSel_P_4_MidwaterTrawl(2)_BLK7repl_2011",
+    ref_row = "SizeSel_P_4_MidwaterTrawl(2)_BLK7repl_2002"
+  ) |> 
+  insert_row(
+    new_row = "SizeSel_P_6_MidwaterTrawl(2)_BLK7repl_2011",
+    ref_row = "SizeSel_P_6_MidwaterTrawl(2)_BLK7repl_2002"
+  ) |> 
+  insert_row(
+    new_row = "SizeSel_PRet_3_MidwaterTrawl(2)_BLK7repl_2011",
+    ref_row = "SizeSel_PRet_3_MidwaterTrawl(2)_BLK7repl_2002"
+  )
+
+SS_writectl(ctrl, paste0(Block7dir, "/2025widow.ctl"), overwrite = TRUE)
+
+r4ss::run(
+  dir = Block7dir,
+  exe = Block7exe,
+  extras = "-nohess",
+  show_in_console = TRUE,
+  skipfinished = TRUE
+)
+
+SS_plots(
+  SS_output(Block7dir, covar=TRUE), 
+  dir = Block7dir, #basedir, 
+  printfolder = "plots", 
+  plot = c(1:21, 23:26)
+)
+
+# Compare -------------------------------------------------
+
+#Comparison plots produced by r4ss
+models <- SSgetoutput(dirvec = c(basedir, testdir), getcovar = TRUE)
+models_ss <- SSsummarize(Models)
+model_names <- c("data updates", "ctrl updates")
+
+labels <- c(
+  "Year", "Spawning biomass (t)", "Relative spawning biomass", "Age-0 recruits (1,000s)",
+  "Recruitment deviations", "Index", "Log index", "1 - SPR", "Density","Management target", 
+  "Minimum stock size threshold", "Spawning output", "Harvest rate"
+)
+
+dir.create(plot_dir <- here("scratch", "ctrl_updates_test"))
+
+SSplotComparisons(
+  models_ss,
+  print = TRUE,
+  plotdir = plot_dir,
+  labels = labels,
+  densitynames = c("SSB_2019","SSB_Virgin"),
+  legendlabels = model_names,
+  indexPlotEach = TRUE,
+  filenameprefix = "sens_base_adj_"
+)

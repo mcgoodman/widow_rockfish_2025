337de4521c36bae5d05a404762ad7f3fb0b74ad0
agdedrick <agdedrick2@gmail.com>
2025-04-30 10:01:46 -0700

    100 jitters on 2025 base model

--- R/jitters.R
+++ R/jitters.R
@@ -12,14 +12,14 @@ source(here("R", "functions", "bridging_functions.R"))
 
 # Set directories
 base_dir <- here("models", "2025 base model")  # base model directory
-jitter_dir <- here("models", "jitters")  # directory to house jitters, in case base model updates again
+jitter_dir <- here("models", "jitters")  # directory to house jitters
 
 # Copy base model into jitter folder
-copy_SS_inputs(dir_old = base_dir, dir_new = jitter_dir) 
+copy_SS_inputs(dir.old = base_dir, dir.new = jitter_dir, overwrite = TRUE) 
 
 # Set number of jitters and jitter fraction
-njitters <- 3  # number of jitters to run
-jitter_frac <- 0.1  # common value to use according to r4ss documentation, investigate more?
+njitters <- 100  # number of jitters to run
+jitter_frac <- 0.1  # common value to use according to r4ss documentation (https://rdrr.io/github/r4ss/r4ss/man/jitter.html), investigate more?
 
 # Set exe
 base_exe <- "ss3"  # name of exe (set_ss3_exe doesn't work on my windows computer...)
@@ -27,7 +27,7 @@ base_exe <- "ss3"  # name of exe (set_ss3_exe doesn't work on my windows compute
 
 # Run base model ---------------------------------------------------------------
 r4ss::run(
-  dir = base_model_dir,
+  dir = base_dir,
   exe = base_exe,
   skipfinished = FALSE,
   show_in_console = TRUE,
@@ -35,7 +35,7 @@ r4ss::run(
   extras = "-nohess"
 )
 
-base <- SS_output(base_model_dir)
+base <- SS_output(base_dir)
 
 
 # Run jitters ------------------------------------------------------------------
@@ -44,27 +44,39 @@ jitter_loglike <- jitter(
   dir = jitter_dir,
   Njitter = njitters,
   printlikes = TRUE,
-  jitter_fraction = 0.1,  # this is the value suggested in r4ss documentation (https://rdrr.io/github/r4ss/r4ss/man/jitter.html), should look into more
+  jitter_fraction = jitter_frac,  
   exe = base_exe,
   verbose = TRUE,
-  extras = "-nohess"
+  extras = "-nohess",
+  skipfinished = FALSE,
+  show_in_console = TRUE
 )
+# could use init_values_src - lets you decide whether to jitter from the par file, might be worth using if convergence issues
+
+saveRDS(jitter_loglike, here("models/jitters", "jitter_loglike.RDS"))
 
 
 # Compare likelihoods ----------------------------------------------------------
-like_df <- data.frame(run = c("base", seq(1:njitters)), like = NA)  # data frame with run number and likelihood
+like_df <- data.frame(run = seq(1:njitters), like = NA)  # data frame with run number and likelihood
 
-# Put in base
-like_df$like[1] <- base$likelihoods_used$values[1]  # has to be a way to specifically call the row "TOTAL"....
-like_df$like[2:(njitters+1)] <- jitter_loglike
+base_like <- base$likelihoods_used$values[1]  # has to be a way to specifically call the row "TOTAL"....
+
+like_df$like <- jitter_loglike
 
 # Plot
 ggplot(data = like_df, aes(x = run, y = like)) +
   geom_point() +
-  geom_hline(yintercept = base$likelihoods_used$values[1]) +  # horizontal line at base ll
+  geom_hline(yintercept = base_like) +  # horizontal line at base ll
   xlab("log likelihood") +
-  theme_bw()
+  theme_bw() +
+  theme(axis.text.x = element_blank())
 
 ggsave(here("figures/diagnostics", "jitter_comparison.png"), width = 6, height = 5, units = "in")
 
+
+# See what likelihoods were below the base
+run_summary <- data.frame(run = 1:njitters, loglike = jitter_loglike)
+below_base <- run_summary %>%
+  filter(loglike < base_like)
+min(jitter_loglike)  # lowest loglikelihood of 7905.46 (base is 7912.2 for this run)
 

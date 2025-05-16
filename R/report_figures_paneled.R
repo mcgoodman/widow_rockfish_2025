eaaeea7f667e011dd602167ce9172615cb604af7
Maurice Codespoti Goodman <goodman.maurice@gmail.com>
2025-05-16 13:14:29 -0400

    figure tweaks, moving some of Laurie's figure code over

--- R/report_figures_paneled.R
+++ R/report_figures_paneled.R
@@ -1,7 +1,8 @@
 
 library("r4ss")
-library("dplyr")
-library("ggplot2")
+library("tidyverse")
+library("nwfscSurvey")
+library("cowplot")
 library("magick")
 library("here")
 
@@ -239,6 +240,20 @@ panel2 <- image_read(here("figures", "diagnostics", "Like_profile_Male_M.png"))
 combined <- image_append(c(panel1, panel2), stack=TRUE)
 image_write(combined, path = here("figures", "diagnostics", "Nat_Mort.png"))
 
+# Decision table plot -----------------------------------------------
+
+pnl_cc <- image_read(here("figures", "decision_table", "cc_plots", "compare4_Bratio_uncertainty.png"))
+pnl_cc <- pnl_cc |> image_annotate("Constant catch", size = 48, location = "+200+0")
+
+pnl_25 <- image_read(here("figures", "decision_table", "25_plots", "compare4_Bratio_uncertainty.png"))
+pnl_25 <- pnl_25 |> image_annotate("ACL = p*0.25", size = 48, location = "+200+0")
+
+pnl_45 <- image_read(here("figures", "decision_table", "45_plots", "compare4_Bratio_uncertainty.png"))
+pnl_45 <- pnl_45 |> image_annotate("ACL = p*0.45", size = 48, location = "+200+0")
+
+combined <- image_append(c(image_append(c(pnl_cc, pnl_25)), pnl_45), stack = TRUE)
+image_write(combined, path = here("figures", "decision_table", "combined_ssb_with_interval.png"))
+
 # Appendix A: bottom trawl retained length comps --------------------
 
 panel1 <- image_read(file.path(plotdir, "plots", "comp_lenfit_flt1mkt2_page1.png"))
@@ -296,3 +311,124 @@ panel2 <- image_read(file.path(plotdir, "plots", "comp_agefit_flt3mkt0_page2.png
 
 combined1 <- image_append(c(panel1, panel2), stack = TRUE)  # side-by-side
 image_write(combined1, path = file.path(plotdir, "App_A", "hake_agefit.png"))
+
+# sdmTMB-based biomass index ----------------------------------------
+
+dat_2019 <- SS_readdat(here("models", "2019 base model", "Base_45", "2019widow.dat"))
+widow_indices <- dat_2019$CPUE
+
+index_gm <- read.csv("data_provided/WCGBTS/delta_gamma/index/est_by_area.csv")
+index_gm$source <- "sdmTMB delta-\ngamma, 2025"
+index_gm <- index_gm %>% filter(area == "Coastwide")
+
+index_ln <- read.csv("data_provided/WCGBTS/delta_lognormal/index/est_by_area.csv")
+index_ln$source <- "sdmTMB delta-\nlognorm, 2025"
+index_ln <- index_ln %>% filter(area == "Coastwide")
+
+# reorganize old data
+old_index <- widow_indices %>% filter(index == 8) %>%
+  mutate(model = factor(ifelse(year < 1900, "GLMM", "VAST")))
+old_index$source <- old_index$model
+old_index$year <- abs(old_index$year)
+
+# new data v. VAST/GLMM
+# combine the data frames
+index_ln$obs <- index_ln$est; index_ln$se_log <- index_ln$se
+index_gm$obs <- index_gm$est; index_gm$se_log <- index_gm$se
+all_dat <- rbind(old_index[, c(1, 4, 5, 7)],index_ln[, c(2, 10, 11, 9)], index_gm[, c(2, 10, 11, 9)])
+
+dodge_width <- 0.9  # Adjust this value to control spacing
+surv_comp_plot <- ggplot(all_dat, aes(year, obs, col = source)) + 
+  geom_errorbar(data = all_dat, aes(ymin = qlnorm(.025, log(obs), sd = se_log), 
+                                    ymax = qlnorm(.975, log(obs), sd = se_log)), 
+                position = position_dodge(width = dodge_width)) + 
+  geom_line(position = position_dodge(width = dodge_width)) +
+  geom_point(position = position_dodge(width = dodge_width)) +
+  labs(y = "index", col = "Model:") + 
+  theme_bw() + theme(legend.position = "bottom") + 
+  scale_color_manual(values = c("gray","black", "#005BFF", "#FF592F"))
+
+
+ggsave(
+  here("figures", "WCGBTS_survey", "survey_comparision_plot.png"), 
+  surv_comp_plot, dpi = 300,  
+  width = 5, height = 4, units = "in"
+)
+
+# length-at-age comps -----------------------------------------------
+
+species <- "widow rockfish"
+survey <- "NWFSC.Combo"
+
+age_bins <- 0:40
+length_bins <- seq(8, 56, by = 2)
+
+strata <- nwfscSurvey::CreateStrataDF.fn(
+  names = c("South - shallow", "South - deep", "North - shallow", "North - deep"), 
+  depths.shallow = c(  55,  183,  55, 183),
+  depths.deep    = c( 183,  400, 183, 400),
+  lats.south     = c(34.5, 34.5,  40.5,  40.5),
+  lats.north     = c(  40.5,   40.5,  49,  49)
+)
+
+# Pull scientific catch survey data
+catch <- nwfscSurvey::pull_catch(common_name = species, survey = survey)
+
+# Age, length, weight
+bio <- nwfscSurvey::pull_bio(common_name = species, survey = survey)
+
+# caal plots
+caal <- get_raw_caal(
+  data = bio, 
+  len_bins = length_bins,
+  age_bins = age_bins
+)
+
+caal_fm <- caal %>% select(year, Lbin_lo, Lbin_hi, starts_with("f"), -fleet) %>% 
+  pivot_longer(cols = starts_with("f"), names_to = "age", values_to = "count") %>%
+  mutate(age = as.integer(gsub("f", "", age)))
+
+caal_ml <- caal %>% select(year, Lbin_lo, Lbin_hi, starts_with("m"), -month, -fleet) %>% 
+  pivot_longer(cols = starts_with("m"), names_to = "age", values_to = "count") %>%
+  mutate(age = as.integer(gsub("m", "", age)))
+
+caal_fm_plot <- ggplot(caal_fm, aes(age, Lbin_lo, size = log(count+1))) + 
+  geom_point(col = "#FF592F", pch = 'o') + facet_wrap(~year) + 
+  labs(x = "Age", y = "Length") + theme_bw() + 
+  scale_size_continuous(range = c(0.5, 6)) + guides(size="none")
+
+caal_ml_plot <- ggplot(caal_ml, aes(age, Lbin_lo, size = log(count+1))) + 
+  geom_point(col = "#005BFF", pch = 'o') + facet_wrap(~year) + 
+  labs(x = "Age", y = "Length") + theme_bw() + 
+  scale_size_continuous(range = c(0.5, 6)) + guides(size="none")
+
+caal_WCGBTS <- plot_grid(caal_fm_plot, caal_ml_plot, nrow = 2)
+
+ggsave(here("figures", "WCGBTS_survey", 'caal_WCGBTS_plot.png'), caal_WCGBTS, dpi = 300,  
+       width = 7, height = 10, units = "in")
+
+length_comps <- get_expanded_comps(
+  bio_data = bio,
+  catch_data = catch,
+  comp_bins = length_bins,
+  strata = strata,
+  comp_column_name = "length_cm",
+  output = "full_expansion_ss3_format",
+  two_sex_comps = TRUE,
+  input_n_method = "stewart_hamel"
+)
+
+plot_comps(length_comps, dir = here("figures", "WCGBTS_survey"))
+
+age_comps <- get_expanded_comps(
+  bio_data = bio,
+  catch_data = catch,
+  comp_bins = age_bins,
+  strata = strata,
+  comp_column_name = "age",
+  output = "full_expansion_ss3_format",
+  two_sex_comps = TRUE,
+  input_n_method = "stewart_hamel"
+)
+
+plot_comps(age_comps, dir = here("figures", "WCGBTS_survey", "age_plots"))
--- scratch/assorted_figures.R
+++ scratch/assorted_figures.R
@@ -8,126 +8,8 @@ library(tidyverse)
 library(cowplot)
 library(readxl)
 
-# sdmTMB-based biomass index ------------------------------
-plot_save_dir <- here("figures", "WCGBTS_survey")
-
-dat_2019 <- SS_readdat(here("models", "2019 base model", "Base_45", "2019widow.dat"))
-widow_indices <- dat_2019$CPUE
-  
-index_gm <- read.csv("data_provided/WCGBTS/delta_gamma/index/est_by_area.csv")
-index_gm$source <- "sdmTMB delta-\ngamma, 2025"
-index_gm <- index_gm %>% filter(area == "Coastwide")
-
-index_ln <- read.csv("data_provided/WCGBTS/delta_lognormal/index/est_by_area.csv")
-index_ln$source <- "sdmTMB delta-\nlognorm, 2025"
-index_ln <- index_ln %>% filter(area == "Coastwide")
-
-# reorganize old data
-old_index <- widow_indices %>% filter(index == 8) %>%
-  mutate(model = factor(ifelse(year < 1900, "GLMM", "VAST")))
-old_index$source <- old_index$model
-old_index$year <- abs(old_index$year)
-
-# new data v. VAST/GLMM
-# combine the data frames
-index_ln$obs <- index_ln$est; index_ln$se_log <- index_ln$se
-index_gm$obs <- index_gm$est; index_gm$se_log <- index_gm$se
-all_dat <- rbind(old_index[, c(1, 4, 5, 7)],index_ln[, c(2, 10, 11, 9)], index_gm[, c(2, 10, 11, 9)])
-
-dodge_width <- 0.9  # Adjust this value to control spacing
-surv_comp_plot <- ggplot(all_dat, aes(year, obs, col = source)) + 
-  geom_errorbar(data = all_dat, aes(ymin = qlnorm(.025, log(obs), sd = se_log), 
-                                      ymax = qlnorm(.975, log(obs), sd = se_log)), 
-                position = position_dodge(width = dodge_width)) + 
-  geom_line(position = position_dodge(width = dodge_width)) +
-  geom_point(position = position_dodge(width = dodge_width)) +
-  labs(y = "index", col = "Model:") + 
-  theme_bw() + theme(legend.position = "bottom") + 
-  scale_color_manual(values = c("gray","black", "#005BFF", "#FF592F"))
-
-
-ggsave(file.path(plot_save_dir, 'survey_comparision_plot.png'),  dpi = 300,  
-       width = 5, height = 4, units = "in")
-
-# length-at-age comps ------------------------------
-
-species <- "widow rockfish"
-survey <- "NWFSC.Combo"
-
-age_bins <- 0:40
-length_bins <- seq(8, 56, by = 2)
-
-strata <- nwfscSurvey::CreateStrataDF.fn(
-  names = c("South - shallow", "South - deep", "North - shallow", "North - deep"), 
-  depths.shallow = c(  55,  183,  55, 183),
-  depths.deep    = c( 183,  400, 183, 400),
-  lats.south     = c(34.5, 34.5,  40.5,  40.5),
-  lats.north     = c(  40.5,   40.5,  49,  49)
-)
-
-# Pull scientific catch survey data
-catch <- nwfscSurvey::pull_catch(common_name = species, survey = survey)
-
-# Age, length, weight
-bio <- nwfscSurvey::pull_bio(common_name = species, survey = survey)
-
-# caal plots
-caal <- get_raw_caal(
-  data = bio, 
-  len_bins = length_bins,
-  age_bins = age_bins
-)
-
-caal_fm <- caal %>% select(year, Lbin_lo, Lbin_hi, starts_with("f"), -fleet) %>% 
-  pivot_longer(cols = starts_with("f"), names_to = "age", values_to = "count") %>%
-  mutate(age = as.integer(gsub("f", "", age)))
-
-caal_ml <- caal %>% select(year, Lbin_lo, Lbin_hi, starts_with("m"), -month, -fleet) %>% 
-  pivot_longer(cols = starts_with("m"), names_to = "age", values_to = "count") %>%
-  mutate(age = as.integer(gsub("m", "", age)))
-
-caal_fm_plot <- ggplot(caal_fm, aes(age, Lbin_lo, size = log(count+1))) + 
-  geom_point(col = "#FF592F", pch = 'o') + facet_wrap(~year) + 
-  labs(x = "Age", y = "Length") + theme_bw() + 
-  scale_size_continuous(range = c(0.5, 6)) + guides(size="none")
-
-caal_ml_plot <- ggplot(caal_ml, aes(age, Lbin_lo, size = log(count+1))) + 
-  geom_point(col = "#005BFF", pch = 'o') + facet_wrap(~year) + 
-  labs(x = "Age", y = "Length") + theme_bw() + 
-  scale_size_continuous(range = c(0.5, 6)) + guides(size="none")
-
-caal_WCGBTS <- plot_grid(caal_fm_plot, caal_ml_plot, nrow = 2)
-
-ggsave(file.path(plot_save_dir, 'caal_WCGBTS_plot.png'),  dpi = 300,  
-       width = 7, height = 10, units = "in")
-
-length_comps <- get_expanded_comps(
-  bio_data = bio,
-  catch_data = catch,
-  comp_bins = length_bins,
-  strata = strata,
-  comp_column_name = "length_cm",
-  output = "full_expansion_ss3_format",
-  two_sex_comps = TRUE,
-  input_n_method = "stewart_hamel"
-)
-
-plot_comps(length_comps, dir = file.path(plot_save_dir))
-
-age_comps <- get_expanded_comps(
-  bio_data = bio,
-  catch_data = catch,
-  comp_bins = age_bins,
-  strata = strata,
-  comp_column_name = "age",
-  output = "full_expansion_ss3_format",
-  two_sex_comps = TRUE,
-  input_n_method = "stewart_hamel"
-)
-
-plot_comps(age_comps, dir = file.path(plot_save_dir, "age_plots"))
-
 # weight/length and length/age things: ------------------------------
+
 plot_save_dir <- here("figures", "length_age_weight")
 
 # age-length plot: figure 35
@@ -154,7 +36,33 @@ surv_agelen$fleet <- rep("NWFSC") # no other data w Lbin_lo > 0
 
 # pacfin data **ADD**
 # read in here
-pfin_dat <- bds_cleaned_all|>filter(!is.na(Age))
+
+load(here("data_provided", "PacFIN", "PacFIN.WDOW.bds.25.Mar.2025.RData"))
+
+used_gears <- c("TWL","NET","MSC","POT","HKL")
+good_lengths <- c("F", "T", "U")
+good_methods <- c("R")
+good_samples <- c("C","M","NA")
+good_states <- c("WA", "OR", "CA")
+good_age_method <- c("BB","B","U","NA")
+
+bds_cleaned <- bds.pacfin |> 
+  filter(SAMPLE_YEAR >= 2005) |> #Only do post 2005 data
+  cleanPacFIN(
+    keep_gears = used_gears,         
+    CLEAN = TRUE,
+    keep_age_method = good_age_method,
+    keep_sample_type = good_samples,
+    keep_sample_method = good_methods,
+    keep_length_type = good_lengths,
+    keep_states = good_states,
+    spp = "widow rockfish"
+  )  |> 
+  left_join(gear_groups_2024,by = "AGENCY_SAMPLE_NUMBER")|> ##a ppend the gear_groups
+  dplyr::mutate(stratification = paste(state,gear_group,sep = ".")) |> # Stratification is a combination of gear and state (matches catch data formatting)
+  dplyr::filter(!PACFIN_GEAR_NAME %in% c("XXX","OTH-KNOWN","DNSH SEINE"))
+
+pfin_dat <- bds_cleaned_all |> filter(!is.na(Age))
 pfin_dat$fleet <- rep("PACFIN")
 
 # ashop age and legnth etc.
@@ -176,21 +84,21 @@ all_agelen$widow_sex <- ifelse(all_agelen$widow_sex == "M", "male", all_agelen$w
 age_range = seq(from = min(all_agelen$widow_age, na.rm = T), to = max(all_agelen$widow_age, na.rm = T), length.out = 100)
 base_model_ins <- SS_read(file.path(here("models", "2025 base model")), 
                       ss_new = FALSE)
-vblFk <- base_model_ins$ctl$MG_parms["VonBert_K_Fem_GP_1", ]$INIT
-vblFMin <- base_model_ins$ctl$MG_parms["L_at_Amin_Fem_GP_1", ]$INIT
-vblFMax <- base_model_ins$ctl$MG_parms["L_at_Amax_Fem_GP_1", ]$INIT
-vblMk <- base_model_ins$ctl$MG_parms["VonBert_K_Mal_GP_1", ]$INIT
-vblMMin <- base_model_ins$ctl$MG_parms["L_at_Amin_Mal_GP_1", ]$INIT
-vblMMax <- base_model_ins$ctl$MG_parms["L_at_Amax_Mal_GP_1", ]$INIT
+vblFk <- base_ctl$MG_parms["VonBert_K_Fem_GP_1", ]$INIT
+vblFMin <- base_ctl$MG_parms["L_at_Amin_Fem_GP_1", ]$INIT
+vblFMax <- base_ctl$MG_parms["L_at_Amax_Fem_GP_1", ]$INIT
+vblMk <- base_ctl$MG_parms["VonBert_K_Mal_GP_1", ]$INIT
+vblMMin <- base_ctl$MG_parms["L_at_Amin_Mal_GP_1", ]$INIT
+vblMMax <- base_ctl$MG_parms["L_at_Amax_Mal_GP_1", ]$INIT
 fm_vbl_in <- vblFMax - (vblFMax - vblFMin)*exp(-vblFk*age_range)
 ml_vbl_in <- vblMMax - (vblMMax - vblMMin)*exp(-vblMk*age_range)
-base_model_outs <- SS_output(file.path(here("models", "2025 base model")))
-vblFk <- base_model_outs$parameters["VonBert_K_Fem_GP_1", ]$Value
-vblFMin <- base_model_outs$parameters["L_at_Amin_Fem_GP_1", ]$Value
-vblFMax <- base_model_outs$parameters["L_at_Amax_Fem_GP_1", ]$Value
-vblMk <- base_model_outs$parameters["VonBert_K_Mal_GP_1", ]$Value
-vblMMin <- base_model_outs$parameters["L_at_Amin_Mal_GP_1", ]$Value
-vblMMax <- base_model_outs$parameters["L_at_Amax_Mal_GP_1", ]$Value
+base_par <- SS_output(file.path(here("models", "2025 base model")))
+vblFk <- base_par$parameters["VonBert_K_Fem_GP_1", ]$Value
+vblFMin <- base_par$parameters["L_at_Amin_Fem_GP_1", ]$Value
+vblFMax <- base_par$parameters["L_at_Amax_Fem_GP_1", ]$Value
+vblMk <- base_par$parameters["VonBert_K_Mal_GP_1", ]$Value
+vblMMin <- base_par$parameters["L_at_Amin_Mal_GP_1", ]$Value
+vblMMax <- base_par$parameters["L_at_Amax_Mal_GP_1", ]$Value
 fm_vbl_out <- vblFMax - (vblFMax - vblFMin)*exp(-vblFk*age_range)
 ml_vbl_out <- vblMMax - (vblMMax - vblMMin)*exp(-vblMk*age_range)
 vbl_lines <- data.frame(widow_age = c(age_range, age_range, age_range, age_range), 
@@ -306,10 +214,10 @@ bF_NW <- weight_length_estimates_NWFSC$B[1]
 aM_NW <- weight_length_estimates_NWFSC$A[2]
 bM_NW <- weight_length_estimates_NWFSC$B[2]
 # values from 2025
-aF_2025 <- base_model_outs$parameters["Wtlen_1_Fem_GP_1", ]$Value
-bF_2025 <- base_model_outs$parameters["Wtlen_2_Fem_GP_1", ]$Value
-aM_2025 <- base_model_outs$parameters["Wtlen_1_Mal_GP_1", ]$Value
-bM_2025 <- base_model_outs$parameters["Wtlen_2_Mal_GP_1", ]$Value
+aF_2025 <- base_par$parameters["Wtlen_1_Fem_GP_1", ]$Value
+bF_2025 <- base_par$parameters["Wtlen_2_Fem_GP_1", ]$Value
+aM_2025 <- base_par$parameters["Wtlen_1_Mal_GP_1", ]$Value
+bM_2025 <- base_par$parameters["Wtlen_2_Mal_GP_1", ]$Value
 # lengths
 len <- seq(from = 1, to = 60, length.out = 100)
 # female lines
@@ -339,7 +247,7 @@ ggplot(NULL, aes(NULL)) +
   labs(lty = "Model", x = "Length", y = "Weight") + 
   theme_bw()
 
-ggsave(file.path(plot_save_dir, 'wl_dat.png'),  dpi = 300,  
+ggsave(file.path(plot_save_dir, 'wl_dat.png'), dpi = 300,  
        width = 5, height = 4, units = "in")
 
 ggplot(line_est, aes(len, value, lty = name, color = name)) + 
@@ -350,7 +258,7 @@ ggplot(line_est, aes(len, value, lty = name, color = name)) +
   labs(lty = "Model", x = "Length", y = "Weight", color = "Estimate") + 
   theme_bw() + theme(legend.position = "bottom") + guides(lty="none")
 
-ggsave(file.path(plot_save_dir, 'wl_dat_2025.png'),  dpi = 300,  
+ggsave(file.path(plot_save_dir, 'wl_dat_2025.png'), dpi = 300,  
        width = 5, height = 4, units = "in")
 
 ggplot(line_est %>% filter(name != "est_2025"), aes(len, value, lty = name, color = name)) + 
@@ -361,7 +269,7 @@ ggplot(line_est %>% filter(name != "est_2025"), aes(len, value, lty = name, colo
   labs(lty = "Model", x = "Length", y = "Weight", color = "Estimate") + 
   theme_bw() + theme(legend.position = "bottom") + guides(lty="none")
 
-ggsave(file.path(plot_save_dir, 'wl_dat_2019.png'),  dpi = 300,  
+ggsave(file.path(plot_save_dir, 'wl_dat_2019.png'), dpi = 300,  
        width = 5, height = 4, units = "in")
 
 
